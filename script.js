79ca3e5dd57df668e766c3e182dcb1020ba32778
Bruno Sayago <brunossgripp@gmail.com>
2022-07-04 19:19:16 -0300

    Requisitos até 10

--- script.js
+++ script.js
@@ -52,6 +52,49 @@ const botaoLimpar = document.getElementById('clear-board');
 
 botaoLimpar.addEventListener('click', retornaCorInicial);
 
+// Gerador de Board
+const quadroPixels = document.getElementById('pixel-board');
+
+function apagaPixels() {
+  while (quadroPixels.children.length !== 0) {
+    quadroPixels.removeChild(quadroPixels.lastChild);
+  }
+}
+
+function criaLinhas(numA) {
+  for (let index = 1; index <= numA; index += 1) {
+    const novaLinha = document.createElement('div');
+    novaLinha.className = 'linha';
+    quadroPixels.appendChild(novaLinha);
+  }
+}
+
+function criaPixels(numA) {
+  const linhas = quadroPixels.children;
+  for (let index = 0; index < linhas.length; index += 1) {
+    for (let index2 = 1; index2 <= numA; index2 += 1) {
+      const pixelCriado = document.createElement('div');
+      pixelCriado.className = 'pixel';
+      linhas[index].appendChild(pixelCriado);
+    }
+  }
+}
+
+function mudaTamanhoPixels() {
+  const input = document.getElementById('board-size').value;
+  const inputNum = parseInt(input, 10);
+  if (input === '') {
+    alert('Board Inválido!');
+  } else {
+    apagaPixels();
+    criaLinhas(inputNum);
+    criaPixels(inputNum);
+  }
+}
+
+const botaoTamanho = document.getElementById('generate-board');
+botaoTamanho.addEventListener('click', mudaTamanhoPixels);
+
 // Função Inicial
 function carregamentoInicial() {
   selecionaCorInicial();
